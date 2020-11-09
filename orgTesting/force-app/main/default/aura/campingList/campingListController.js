f48fbdf65a9d05fc60838c187952ca4bfa9b5c6d
Benet Cosculluela <monica.benet-cosculluela-external@capgemini.com>
2020-11-09 13:55:23 +0100

    Connect components with events

--- orgTesting/force-app/main/default/aura/campingList/campingListController.js
+++ orgTesting/force-app/main/default/aura/campingList/campingListController.js
@@ -1,4 +1,23 @@
 ({
+    
+    //load items from salesforce
+    doInit: function(component, event, helper) {
+        //create the action
+        let action = component.get("c.getItems");
+        //add callback behaviour for when response is received
+        action.setCallback(this, function(response){  //this is the action handler function itself
+            let state  = response.getState();
+            if(state=== "SUCCESS"){
+                component.set("v.items", response.getReturnValue());
+            }
+            else{
+            console.log("failed with state: "+ state);
+            }
+        });
+        //send action off to be executed
+        $A.enqueueAction(action); 
+    },
+    
     clickCreateItem : function(component, event, helper) {
         let validItem = component.find('itemform').reduce(function (validSoFar, inputCmp){
             inputCmp.showHelpMessageIfInvalid();
@@ -7,14 +26,12 @@
         if (validItem){
             let newItem = component.get("v.newItem");
             console.log("Create new item : " + JSON.stringify(newItem));
-            let theItems = component.get("v.items");
-            theItems.push(newItem);
-            component.set("v.items", theItems);
+            helper.createItem(component, newItem);
         }
         component.set("v.newItem", {'sobjectType':'Camping_Item__c', 
-                    'Name':'',
-                    'Quantity__c':0, 
-                    'Price__c': 0,
-                    'Packed__c':false});
+            'Name':'',
+            'Quantity__c':0, 
+            'Price__c': 0,
+            'Packed__c':false});
     }
 })
